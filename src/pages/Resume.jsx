b93d98c62ca6ece56c7f719b6b8a1f3468f60456
Sathish0225 <kumar@a-sat.sg>
2024-07-30 18:32:32 +0800

    Resume Update and Page Number Scroll Implemented

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { BsDownload } from "react-icons/bs";
 import pdf from "../Resume.pdf";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 import { Document, Page, pdfjs } from 'react-pdf';
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 import 'react-pdf/dist/esm/Page/TextLayer.css';
@@ -11,8 +12,13 @@ pdfjs.GlobalWorkerOptions.workerSrc = new URL(
 ).toString();
 
 const Resume = () => {
-  console.log(pdfjs.version);
   const [wid, setwid] = useState(window.innerWidth);
+  const [numPages, setNumPages] = useState();
+  const [pageNumber, setPageNumber] = useState(1);
+
+  const onDocumentLoadSuccess = ({ numPages }) => {
+    setNumPages(numPages);
+  }
 
   const handleResize = () => {
     setwid(window.innerWidth);
@@ -20,6 +26,18 @@ const Resume = () => {
 
   window.addEventListener("load", handleResize);
   window.addEventListener("resize", handleResize);
+
+  const pageBtnStyle = {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: "20px",
+    padding: "5px 10px",
+    backgroundColor: "#431f47",
+    color: "#E7E7E7",
+    borderRadius: "8px",
+  };
 
   return (
     <div className='ResumePage'>
@@ -30,15 +48,16 @@ const Resume = () => {
         </button>
       </Link>
 
-      <Document file={pdf} className="resumeview">
-        <Page pageNumber={1} scale={wid > 786 ? 1.7 : 0.6} />
-      </Document>
-
-      <Link to={pdf} target='_blank' rel="noopener" download="Sathishkumar's Resume">
-        <button className='downloadCV' type='button'>
-          <h3><BsDownload />&nbsp; Download CV</h3>
-        </button>
-      </Link>
+      <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
+        <Document file={pdf} className="resumeview" onLoadSuccess={onDocumentLoadSuccess}>
+          <Page pageNumber={pageNumber} scale={wid > 786 ? 1.7 : 0.6} />
+        </Document>
+        <div style={pageBtnStyle}>
+          <div className="pageBtn" onClick={() => setPageNumber(pageNumber - 1)} style={{ padding: "10px", marginRight: "10px", cursor: "pointer" }}><FaChevronLeft /></div>
+          <div style={{ marginTop: "-5px" }}>{pageNumber} of {numPages}</div>
+          <div className="pageBtn" onClick={() => setPageNumber(pageNumber + 1)} style={{ padding: "10px", marginLeft: "10px", cursor: "pointer" }}><FaChevronRight /></div>
+        </div>
+      </div>
 
     </div>
   )
